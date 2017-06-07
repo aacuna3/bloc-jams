b972ee993b64973fc5a65ff966ff54f6a9bf1219
Alexa Acuna <Alexa Acuna>
2017-06-06 20:06:16 -1000

    checkpoint 19 correction 1

--- scripts/album.js
+++ scripts/album.js
@@ -125,7 +125,7 @@ var updatePlayerBarSong = function() {
   $('.currently-playing .song-name').text(currentSongFromAlbum.title);
   $('.currently-playing .artist-name').text(currentAlbum.artist);
   $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
-  $('main-controls .playpause').html(playerBarPauseButton);
+  $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
 
